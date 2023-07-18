dd05f19549cbb18e07a414ed8d2838aa4c4acc53
jdev <jlacandula@gmail.com>
2023-07-18 14:24:27 +0800

    wip: date filter 3

--- app/src/components/events/DateRangePicker.js
+++ app/src/components/events/DateRangePicker.js
@@ -15,10 +15,21 @@ const DateRangePicker = ({ onChange }) => {
     }
   }, []);
 
+  const convertToPhpDate = (date) => {
+    if (date instanceof Date) {
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
+    }
+    return '';
+  };
+
   const handleRangeChange = (update) => {
+    const convertedUpdate = update.map(convertToPhpDate);
     setDateRange(update);
     if (update[0] && update[1]) {
-      onChange(update);
+      onChange(convertedUpdate);
     }
   };
 
